b31d9ee08633918f21981bbe72a936efbe650e80
notyourbear <pracenberg@gmail.com>
2018-01-07 16:36:28 -0800

    added helper function to check for duplicity of models

--- dev/helpers/functions.js
+++ dev/helpers/functions.js
@@ -7,7 +7,17 @@ const sample = collection => {
   return collection[index]
 }
 
+const checkIfAlreadyGenerated = (model1, model2, simsAllowed = 0) => {
+  const similarities = Object.keys(model1).reduce((sims, key) => {
+    if (key === 'type') return sims
+    return model1[key] === model2[key] ? sims += 1 : sims
+  }, 0)
+
+  return similarities >= simsAllowed
+}
+
 
 module.exports = {
-  sample
+  sample,
+  checkIfAlreadyGenerated
 }
--- dev/helpers/functions.test.js
+++ dev/helpers/functions.test.js
@@ -8,9 +8,31 @@ describe('helper functions', () => {
 
     test('it selects one of the possible selections', () => {
       const options = [1,2,3]
-      const result = helpers.sample(options);
+      const result = helpers.sample(options)
 
-      expect((result === 1 || result === 2 || result === 3)).toBeTruthy();
+      expect((result === 1 || result === 2 || result === 3)).toBeTruthy()
+    })
+  })
+
+  describe('checkIfAlreadyGenerated', () => {
+    test('check equal objects -- returns true', () => {
+      const obj1 = {
+        type: 'person',
+        name: 'testing'
+      }
+      const obj2 = Object.assign({}, obj1)
+
+      expect(helpers.checkIfAlreadyGenerated(obj1, obj2)).toBeTruthy()
+    })
+
+    test('check different objects -- return false', () => {
+      const obj1 = {
+        type: 'person',
+        name: 'testing'
+      }
+      const obj2 = Object.assign({}, obj1, {name: 'another'})
+
+      expect(helpers.checkIfAlreadyGenerated(obj1, obj2, 1)).toBeFalsy()
     })
   })
 })
