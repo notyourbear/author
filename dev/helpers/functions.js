85bf95a932d654a2f007f2370e87d2b7523038c8
notyourbear <pracenberg@gmail.com>
2018-01-08 21:00:44 -0800

    added additional helpers for templating engine

--- dev/helpers/functions.js
+++ dev/helpers/functions.js
@@ -1,11 +1,11 @@
 const functions = {}
 
-
-const sample = collection => {
-  if (typeof collection === 'string') return collection
-  const index = Math.floor(Math.random() * collection.length)
-  return collection[index]
+const between = str => {
+  const options = str.split('-').map(Number)
+  return getRandomInt(options[0], options[1])
 }
+
+const capitalize = str => str[0].toUpperCase() + str.slice(1)
 
 const checkIfAlreadyGenerated = (model1, model2, simsAllowed = 0) => {
   const similarities = Object.keys(model1).reduce((sims, key) => {
@@ -16,8 +16,38 @@ const checkIfAlreadyGenerated = (model1, model2, simsAllowed = 0) => {
   return similarities >= simsAllowed
 }
 
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+const modifier = (str, fnHash = {}) => {
+  const fns = str.split('|')
+
+  const pipe = (input, fnArray) => {
+    const modified =
+      fnHash[fnArray[0]] ? fnHash[fnArray[0]].call(null, input) : input
+    return fnArray.length === 1 ? modified : pipe(modified, fnArray.slice(1))
+  }
+
+  return (input = '') => pipe(input, fns)
+}
+
+const possessive = str => `${str}'s`
+
+const sample = collection => {
+  if (typeof collection === 'string') return collection
+  const index = getRandomInt(0, collection.length)
+  return collection[index]
+}
+
+const uppercase = str => str.toUpperCase()
 
 module.exports = {
+  between,
+  capitalize,
+  checkIfAlreadyGenerated,
+  modifier,
+  possessive,
   sample,
-  checkIfAlreadyGenerated
+  uppercase
 }
--- dev/helpers/functions.test.js
+++ dev/helpers/functions.test.js
@@ -1,16 +1,25 @@
 const helpers = require('./functions')
 
 describe('helper functions', () => {
-  describe('sample', () => {
-    test('it returns input string', () => {
-      expect(helpers.sample('hello')).toBe('hello')
+  describe('between', () => {
+    test('gets 1 if between is 1-1', () => {
+      expect(helpers.between('1-1')).toEqual(1)
     })
 
-    test('it selects one of the possible selections', () => {
-      const options = [1,2,3]
-      const result = helpers.sample(options)
+    test('gets 0 if between is 0-0', () => {
+      expect(helpers.between('0-0')).toEqual(0)
+    })
 
-      expect((result === 1 || result === 2 || result === 3)).toBeTruthy()
+    test('gets a num between 1 and 3 if between is 1-3', () => {
+      const random = helpers.between('1-3')
+      const check = random === 1 || random === 2 || random === 3
+      expect(check).toBeTruthy()
+    })
+  })
+
+  describe('capitalize', () => {
+    test('returns capitalized word', () => {
+      expect(helpers.capitalize('test')).toBe('Test')
     })
   })
 
@@ -33,6 +42,53 @@ describe('helper functions', () => {
       const obj2 = Object.assign({}, obj1, {name: 'another'})
 
       expect(helpers.checkIfAlreadyGenerated(obj1, obj2, 1)).toBeFalsy()
+    })
+  })
+
+  describe('modifier', () => {
+    const fnHash = {
+      capitalize: helpers.capitalize,
+      possessive: helpers.possessive
+    }
+
+    test('should use both modifiers', () => {
+      const result = helpers.modifier('capitalize|possessive', fnHash)('test')
+      expect(result).toBe("Test's")
+    })
+
+    test('should spit out unchanged str if no modifier provided', () => {
+      const result = helpers.modifier('', fnHash)('test')
+      expect(result).toBe("test")
+    })
+
+    test('should not use modifiers if not in hash', () => {
+      const result = helpers.modifier('capitalize|iDontExist', fnHash)('test')
+      expect(result).toBe("Test")
+    })
+  })
+
+  describe('possessive', () => {
+    test('returns a possessive version of a string', () => {
+      expect(helpers.possessive('test')).toBe("test's")
+    })
+  })
+
+  describe('sample', () => {
+    test('it returns input string', () => {
+      expect(helpers.sample('hello')).toBe('hello')
+    })
+
+    test('it selects one of the possible selections', () => {
+      const options = [1,2,3]
+      const result = helpers.sample(options)
+
+      expect((result === 1 || result === 2 || result === 3)).toBeTruthy()
+    })
+  })
+
+  describe('uppercase', () => {
+    test('returns an all caps version of a string', () => {
+      expect(helpers.uppercase('test')).toBe('TEST')
     })
   })
 })
