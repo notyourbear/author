923ec8b0c423ea51da6a5e2c20647e0a6d0ecb38
notyourbear <pracenberg@gmail.com>
2018-01-23 21:01:45 -0800

    start model gen

--- dev/generator/generator.js
+++ dev/generator/generator.js
@@ -1,18 +1,24 @@
-const Model = require('./model/model.js')
-const checkIfAlreadyGenerated = required('../helpers/functions').checkIfAlreadyGenerated
+const Model = require('../model/model.js')
+const Parser = require('../parser/parser.js')
+const Compiler = require('../compiler/compiler.js')
 
-const Generator = (jsonSchema, state = {}) => {
-  const schema = JSON.parse(jsonSchema)
-  const models = schema.model;
-  const grammars = schema.grammar;
+const helpers = required('../helpers/functions')
 
+const Generator = (schema, state = {}) => {
+  // const schema = JSON.parse(jsonSchema)
+  const grammars = schema.grammar
+  const models = Object.keys(schema.model).reduce((state, key) => {
+    state[key] = Model(schema.model[key], helpers)
+  }, {})
 
+  console.log({models})
   // read entry
   // parse / generate grammar
   // check state
   // generate state if necessary
   // generate statement from grammar
-
+  const parsed = Parser(schema.entry, grammars)
+  return models;
 }
 
 
@@ -25,11 +31,23 @@ model: {
 grammar {}
 entry: {}
 
-so; the entry point gets read;
-entry points to a grammar;
-grammar, when coming across a model,
-  checks if it exists in state and uses it,
-  otherwise generates one and adds it to state;
-  person.type.one
-  person.type
+so step one i think will be to rewrite parser to be... easier to append info
+that said, what's next:
+1. parses all models and creates state
+2. read's entry
+3. create array of things to compile from entry
+4. runs through array and compiles or parses as necessary
+  - the issue with this:
+    i need to generate the text as i parse. or rather unpack it into
+    a larger string that i then entirely compile. or compile in batches.
+    i could stream it. i suppose.
+    version one: one gigantic string.
+5. replace as needed
+
+so how do i do this. i need update the parser, first of all:
+it shouldnt just be a function it should be a factory
+  - the factory will have many things:
+    -
 **/
+
+module.exports = Generator;
---
+++ dev/generator/generator.test.js
@@ -0,0 +1,9 @@
+const Generator = require('./generator.js')
+
+const schema = {
+  model: {
+    farmer: {
+      name: ['Patrick', 'Joe']
+    }
+  }
+}
