e16b4742193fd937f6a2ba61d453109e9dbe2f0c
notyourbear <pracenberg@gmail.com>
2018-02-14 19:16:07 -0800

    rollup working as intended now

--- rollup.config.js
+++ rollup.config.js
@@ -3,32 +3,43 @@ const resolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
 const uglify = require('rollup-plugin-uglify')
 const replace = require('rollup-plugin-replace')
+const progress = require('rollup-plugin-progress')
 const builtins = require('rollup-plugin-node-builtins')
 const globals = require('rollup-plugin-node-globals')
+const filesize = require('rollup-plugin-filesize')
 
 export default {
   input: 'dev/generator/generator.js',
   output: {
     file: 'index.js',
     format: 'umd',
-    name: 'Deutung'
+    name: 'Deutung',
+    globals: {
+      pluralize: '$',
+      seedrandom: '$',
+      'fs-jetpack': '$'
+    }
   },
   plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    builtins(),
-    globals(),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-    }),
-    commonjs(),
     replace({
       exclude: 'node_modules/**',
       ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
-    (process.env.NODE_ENV === 'production' && uglify())
+    progress(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+      preferBuiltins: false,
+      modulesOnly: true
+    }),
+    commonjs(),
+    builtins(),
+    globals(),
+    (process.env.NODE_ENV === 'production' && uglify()),
+    filesize()
   ]
 };
